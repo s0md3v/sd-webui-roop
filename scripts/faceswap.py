b9071381a3d3129b6b40de59971b7095013965bc
Somdev Sangwan <s0md3v@gmail.com>
2023-06-22 05:32:29 +0530

    Update faceswap.py

--- scripts/faceswap.py
+++ scripts/faceswap.py
@@ -20,7 +20,6 @@ import os
 def get_models():
     models_path = os.path.join(scripts.basedir(), "models/roop/*")
     models = glob.glob(models_path)
-    models += glob.glob(models_path)
     models = [x for x in models if x.endswith(".onnx") or x.endswith(".pth")]
     return models
 
