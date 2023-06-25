2a51bea0e243a85c6ebe47cf28df984062f173f4
nickthelegend <82558763+nickthelegend@users.noreply.github.com>
2023-06-25 12:54:18 +0400

    Update cimage.py
    NSFW broken ig

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -5,5 +5,5 @@ def convert_to_sd(img):
     shapes = []
     chunks = detect(img)
     for chunk in chunks:
-        shapes.append(chunk["score"] > 0.7)
+        shapes.append(chunk["score"])
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
